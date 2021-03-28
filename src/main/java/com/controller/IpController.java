345da58c001fcaa90bb2b2dd7653090feeafc6e8
Lucia <luciamorenonegro@gmail.com>
2021-03-28 13:43:49 -0300

    method ip data in ip controller

--- src/main/java/com/controller/IpController.java
+++ src/main/java/com/controller/IpController.java
@@ -1,16 +1,28 @@
 package com.controller;
 
+import com.service.IpService;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ip")
 public class IpController {
+    private final IpService ipService;
+
+    @Autowired
+    public IpController(IpService ipService) {
+        this.ipService = ipService;
+    }
 
     @GetMapping
-    public ResponseEntity<String> getIpData() {
+    public ResponseEntity<String> getIpData(@RequestBody JSONObject ipJson) throws Exception {
+        String ip = ipJson.getString("ip");
+        ipService.getInformation(ip);
         return ResponseEntity.ok("Hola!");
     }
 }
