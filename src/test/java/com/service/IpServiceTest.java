67f1151226176a4c5913f906cc8d553f8c72dbb3
Lucia <luciamorenonegro@gmail.com>
2021-03-28 23:02:45 -0300

    test

--- src/test/java/com/service/IpServiceTest.java
+++ src/test/java/com/service/IpServiceTest.java
@@ -1,4 +1,49 @@
 package com.service;
 
+import com.client.CountryInformationClient;
+import com.client.CurrencyInformationClient;
+import com.client.GeolocationClient;
+import com.model.response.CountryInformationResponse;
+import com.model.response.GeolocationResponse;
+import com.model.response.IpInformation;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class IpServiceTest {
+
+    @InjectMocks
+    private IpService ipService;
+
+    @Mock
+    private CountryInformationClient countryInformationClient;
+
+    @Mock
+    private CurrencyInformationClient currencyInformationClient;
+
+    @Mock
+    private GeolocationClient geolocationClient;
+
+    @Test
+    public void testGetIpInformation() throws Exception {
+        String ip = "192.168.1.1";
+
+        GeolocationResponse geolocationResponse = new GeolocationResponse();
+        geolocationResponse.setCountryName("Uruguay");
+        geolocationResponse.setCountryCode("uy");
+
+        when(geolocationClient.getGeolocationClient(eq(ip))).thenReturn(geolocationResponse);
+
+        IpInformation info = ipService.getInformation(ip);
+
+        assertEquals("Uruguay", info.getCountry());
+    }
 }
